e439fe926f94d5a5a035ad66c98b3c268da39dd1
CHS96 <sorjfkrh5078@gmail.com>
2020-12-31 14:36:51 +0900

    게시글 수정 및 삭제 업데이트 다만, 삭제 부분에서 500 error가 발생하는데 이유를 모르겠다.

--- src/main/java/com/hanseul/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hanseul/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,15 @@ package com.hanseul.book.springboot.service.posts;
 
 import com.hanseul.book.springboot.domain.posts.Posts;
 import com.hanseul.book.springboot.domain.posts.PostsRepository;
+import com.hanseul.book.springboot.web.dto.PostsListResponseDto;
 import com.hanseul.book.springboot.web.dto.PostsResponseDto;
 import com.hanseul.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hanseul.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +34,20 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
