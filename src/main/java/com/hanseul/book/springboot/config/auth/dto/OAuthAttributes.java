dfa60c97c948e9dece9935ed4d0bd5a96ad2003b
CHS96 <sorjfkrh5078@gmail.com>
2021-01-04 17:35:42 +0900

    네이버 로그인 연동, test오류는 junit 버전이 이상한지 잘안됨

--- src/main/java/com/hanseul/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/hanseul/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,16 +24,32 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> atributes){
-        return ofGoogle(userNameAttributeName, atributes);
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> atributes){
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
         return OAuthAttributes.builder()
-                .name((String) atributes.get("name"))
-                .email((String) atributes.get("email"))
-                .picture((String) atributes.get("picture"))
-                .attributes(atributes)
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
